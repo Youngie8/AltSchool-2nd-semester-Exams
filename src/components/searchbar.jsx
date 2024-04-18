1e36b8aa51b8ab54319892563a7f9b3570f6c1a6
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-04-18 21:22:28 +0100

    emptied the sarch bar

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
 
-const Searchbar = ({ repos }) => {
-  const handleSubmit = (e) => e.preventDefault();
+const Searchbar = () => {
 
-  const handleSearchChange = (e) => {
-    const results = repos.filter(repo => repo.name.includes(e.target.value))
-  }
+
 
   return (
     <div className=""> 
-    <form onSubmit={handleSubmit} className="repos-search-header">
-        <input 
-        type="search"
-        className='repos-search'
-        placeholder='Search for a repo...'
-        onChange={handleSearchChange}
-        />
-        <button className='btn color-2-black-btn'>search</button>
-    </form>
+    Not sure, ill use you.... 
     </div>
   )
 }
